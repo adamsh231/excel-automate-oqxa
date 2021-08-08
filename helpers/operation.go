93f8b8028169358e7c39b92cf33c19a56aa51943
Adam Syarif Hidayatullah <adamsyarif219@gmail.com>
2021-08-09 06:37:06 +0700

    tinggal penilai

--- helpers/operation.go
+++ helpers/operation.go
@@ -11,6 +11,21 @@ const (
 	CKPTTemplateSheetName       = "CKP-T Template"
 	CKPTTemplatePrefixSheetName = "CKP-T"
 	CKPTStartWriteKegiatan      = 13
+	CKPTPejabatPenilai          = "Tony Suprianto, S.Si., M.A.P."
+	CKTPPejabatPenilaiNIP       = "NIP. 197605212002121002"
+
+	StyleArial            = `{"font":{"family": "Arial"}}`
+	StyleBorder           = `{"border": [{"type": "left","color": "000000","style": 1},{"type": "top","color": "000000","style": 1},{"type": "bottom","color": "000000","style": 1},{"type": "right","color": "000000","style": 1}]}`
+	StyleBoldAndUnderline = `{"font":{"bold": true, "underline":"single", "size": 11, "family": "Arial"}}`
+	StyleCenter           = `{"alignment":{"horizontal": "center"}}`
+	StyleBorderCenter     = `{"alignment":{"horizontal": "center"}, "border": [{"type": "left","color": "000000","style": 1},{"type": "top","color": "000000","style": 1},{"type": "bottom","color": "000000","style": 1},{"type": "right","color": "000000","style": 1}]}`
+
+	StyleJumlah = `{"font":{"family": "Arial"}, "font":{"bold": true}, "alignment":{"horizontal": "center", "vertical": "center"}, "border": [{"type": "left","color": "000000","style": 1},{"type": "top","color": "000000","style": 1},{"type": "bottom","color": "000000","style": 6},{"type": "right","color": "000000","style": 1}], "fill":{"type":"pattern","color":["#BFBFBF"],"pattern":1}}`
+)
+
+var (
+	CKPTSatuanOrganisasi = fmt.Sprintf("Satuan Organisasi%3s: %s", " ", "BPS Kabupaten Lamandau")
+	CKPTPeriode          = fmt.Sprintf("Periode%20s: %s", " ", "")
 )
 
 func CreateSheets(file *excelize.File, names []string) (indexes []int) {
@@ -44,12 +59,17 @@ func WriteSpecificCKPT(file *excelize.File, pegawai []map[string]string, kegiata
 		// init column number
 		var columnNumber int
 		var counter int
+		arial, _ := file.NewStyle(StyleArial)
+		border, _ := file.NewStyle(StyleBorder)
+		boldAndUnderline, _ := file.NewStyle(StyleBoldAndUnderline)
+		//center, _ := file.NewStyle(StyleCenter)
+		borderCenter, _ := file.NewStyle(StyleBorderCenter)
+		jumlah, _ := file.NewStyle(StyleJumlah)
 
 		// sheet name
 		sheetName := fmt.Sprintf("%s %s", CKPTTemplatePrefixSheetName, val["nama"])
 
 		// sheet font family
-		arial, _ := file.NewStyle(`{"font":{"family": "Arial"}}`)
 		file.SetCellStyle(sheetName, "A12", "I100", arial)
 
 		for _, detailPegawai := range pegawai {
@@ -60,13 +80,15 @@ func WriteSpecificCKPT(file *excelize.File, pegawai []map[string]string, kegiata
 				jabatanPegawai := fmt.Sprintf("Jabatan%20s: %s", " ", detailPegawai["jabatan"])
 
 				// write
+				file.SetCellValue(sheetName, "B4", CKPTSatuanOrganisasi)
 				file.SetCellValue(sheetName, "B5", namaLengkapPegawai)
 				file.SetCellValue(sheetName, "B6", jabatanPegawai)
+				file.SetCellValue(sheetName, "B7", CKPTPeriode)
 
 				// Utama
 				file.SetCellValue(sheetName, "A12", "UTAMA")
-				boldAndUnderline, _ := file.NewStyle(`{"font":{"bold": true, "size": 11, "family": "Arial"}}`)
 				file.SetCellStyle(sheetName, "A12", "A12", boldAndUnderline)
+				file.SetCellStyle(sheetName, "B12", "I12", border)
 
 				// write kegiatan
 				counter = 0
@@ -74,12 +96,19 @@ func WriteSpecificCKPT(file *excelize.File, pegawai []map[string]string, kegiata
 				for _, detailKegiatan := range val["kegiatan"].([]map[string]string) {
 					if detailKegiatan["fungsi"] == detailPegawai["seksi_utama"] {
 
-						// write
 						columnNumber += 1
+
+						// write
 						file.SetCellValue(sheetName, fmt.Sprintf("A%d", CKPTStartWriteKegiatan+counter), columnNumber)
 						file.SetCellValue(sheetName, fmt.Sprintf("B%d", CKPTStartWriteKegiatan+counter), detailKegiatan["kegiatan"])
 						file.SetCellValue(sheetName, fmt.Sprintf("C%d", CKPTStartWriteKegiatan+counter), detailKegiatan["satuan"])
 						file.SetCellValue(sheetName, fmt.Sprintf("D%d", CKPTStartWriteKegiatan+counter), detailKegiatan["target"])
+
+						// add border
+						file.SetCellStyle(sheetName, fmt.Sprintf("A%d", CKPTStartWriteKegiatan+counter), fmt.Sprintf("I%d", CKPTStartWriteKegiatan+counter), border)
+						file.SetCellStyle(sheetName, fmt.Sprintf("A%d", CKPTStartWriteKegiatan+counter), fmt.Sprintf("A%d", CKPTStartWriteKegiatan+counter), borderCenter)
+						file.SetCellStyle(sheetName, fmt.Sprintf("C%d", CKPTStartWriteKegiatan+counter), fmt.Sprintf("I%d", CKPTStartWriteKegiatan+counter), borderCenter)
+
 						counter += 1
 
 					}
@@ -87,14 +116,17 @@ func WriteSpecificCKPT(file *excelize.File, pegawai []map[string]string, kegiata
 
 				// add cell - if 0
 				if columnNumber == 0 {
-					file.SetCellValue(sheetName, fmt.Sprintf("A%d", CKPTStartWriteKegiatan+counter), "1")
-					file.SetCellValue(sheetName, fmt.Sprintf("B%d", CKPTStartWriteKegiatan+counter), "-")
+					file.SetCellValue(sheetName, fmt.Sprintf("A%d", CKPTStartWriteKegiatan+counter), columnNumber+1)
+					file.SetCellStyle(sheetName, fmt.Sprintf("A%d", CKPTStartWriteKegiatan+counter), fmt.Sprintf("I%d", CKPTStartWriteKegiatan+counter), border)
+					file.SetCellStyle(sheetName, fmt.Sprintf("A%d", CKPTStartWriteKegiatan+counter), fmt.Sprintf("A%d", CKPTStartWriteKegiatan+counter), borderCenter)
+					file.SetCellStyle(sheetName, fmt.Sprintf("C%d", CKPTStartWriteKegiatan+counter), fmt.Sprintf("I%d", CKPTStartWriteKegiatan+counter), borderCenter)
 					counter++
 				}
 
 				// Tambahan
 				file.SetCellValue(sheetName, fmt.Sprintf("A%d", CKPTStartWriteKegiatan+counter), "TAMBAHAN")
 				file.SetCellStyle(sheetName, fmt.Sprintf("A%d", CKPTStartWriteKegiatan+counter), fmt.Sprintf("A%d", CKPTStartWriteKegiatan+counter), boldAndUnderline)
+				file.SetCellStyle(sheetName, fmt.Sprintf("B%d", CKPTStartWriteKegiatan+counter), fmt.Sprintf("I%d", CKPTStartWriteKegiatan+counter), border)
 				counter += 1
 
 				// seksi tambahan
@@ -102,12 +134,19 @@ func WriteSpecificCKPT(file *excelize.File, pegawai []map[string]string, kegiata
 				for _, detailKegiatan := range val["kegiatan"].([]map[string]string) {
 					if detailKegiatan["fungsi"] != detailPegawai["seksi_utama"] {
 
-						// write
 						columnNumber += 1
+
+						// write
 						file.SetCellValue(sheetName, fmt.Sprintf("A%d", CKPTStartWriteKegiatan+counter), columnNumber)
 						file.SetCellValue(sheetName, fmt.Sprintf("B%d", CKPTStartWriteKegiatan+counter), detailKegiatan["kegiatan"])
 						file.SetCellValue(sheetName, fmt.Sprintf("C%d", CKPTStartWriteKegiatan+counter), detailKegiatan["satuan"])
 						file.SetCellValue(sheetName, fmt.Sprintf("D%d", CKPTStartWriteKegiatan+counter), detailKegiatan["target"])
+
+						// add border
+						file.SetCellStyle(sheetName, fmt.Sprintf("A%d", CKPTStartWriteKegiatan+counter), fmt.Sprintf("I%d", CKPTStartWriteKegiatan+counter), border)
+						file.SetCellStyle(sheetName, fmt.Sprintf("A%d", CKPTStartWriteKegiatan+counter), fmt.Sprintf("A%d", CKPTStartWriteKegiatan+counter), borderCenter)
+						file.SetCellStyle(sheetName, fmt.Sprintf("C%d", CKPTStartWriteKegiatan+counter), fmt.Sprintf("I%d", CKPTStartWriteKegiatan+counter), borderCenter)
+
 						counter += 1
 
 					}
@@ -115,16 +154,30 @@ func WriteSpecificCKPT(file *excelize.File, pegawai []map[string]string, kegiata
 
 				// add cell - if 0
 				if columnNumber == 0 {
-					file.SetCellValue(sheetName, fmt.Sprintf("A%d", CKPTStartWriteKegiatan+counter), "1")
-					file.SetCellValue(sheetName, fmt.Sprintf("B%d", CKPTStartWriteKegiatan+counter), "-")
+					file.SetCellValue(sheetName, fmt.Sprintf("A%d", CKPTStartWriteKegiatan+counter), columnNumber+1)
+					file.SetCellStyle(sheetName, fmt.Sprintf("A%d", CKPTStartWriteKegiatan+counter), fmt.Sprintf("I%d", CKPTStartWriteKegiatan+counter), border)
+					file.SetCellStyle(sheetName, fmt.Sprintf("A%d", CKPTStartWriteKegiatan+counter), fmt.Sprintf("A%d", CKPTStartWriteKegiatan+counter), borderCenter)
+					file.SetCellStyle(sheetName, fmt.Sprintf("C%d", CKPTStartWriteKegiatan+counter), fmt.Sprintf("I%d", CKPTStartWriteKegiatan+counter), borderCenter)
 					counter++
 				}
+
+				// summary
+				file.SetCellValue(sheetName, fmt.Sprintf("A%d", CKPTStartWriteKegiatan+counter), "JUMLAH")
+				file.MergeCell(sheetName, fmt.Sprintf("A%d", CKPTStartWriteKegiatan+counter), fmt.Sprintf("B%d", CKPTStartWriteKegiatan+counter))
+				file.SetCellStyle(sheetName, fmt.Sprintf("A%d", CKPTStartWriteKegiatan+counter), fmt.Sprintf("G%d", CKPTStartWriteKegiatan+counter), jumlah)
+
+				file.SetCellValue(sheetName, fmt.Sprintf("B%d", CKPTStartWriteKegiatan+counter+2), "Kesepakatan Target")
+				file.SetCellValue(sheetName, fmt.Sprintf("B%d", CKPTStartWriteKegiatan+counter+3), "Tanggal:")
+				file.SetCellValue(sheetName, fmt.Sprintf("B%d", CKPTStartWriteKegiatan+counter+3), "Pegawai Yang Dinilai")
+				file.SetCellValue(sheetName, fmt.Sprintf("B%d", CKPTStartWriteKegiatan+counter+6), detailPegawai["nama"])
+				file.SetCellValue(sheetName, fmt.Sprintf("B%d", CKPTStartWriteKegiatan+counter+7), fmt.Sprintf("NIP. %s", detailPegawai["nip"]))
+				file.SetCellValue(sheetName, fmt.Sprintf("E%d", CKPTStartWriteKegiatan+counter+3), "Pejabat Penilai")
+				file.SetCellValue(sheetName, fmt.Sprintf("E%d", CKPTStartWriteKegiatan+counter+6), CKPTPejabatPenilai)
+				file.SetCellValue(sheetName, fmt.Sprintf("E%d", CKPTStartWriteKegiatan+counter+7), CKTPPejabatPenilaiNIP)
 
 			}
 		}
 
-		border, _ := file.NewStyle(`{"border": [{"type": "left","color": "000000","style": 1},{"type": "top","color": "000000","style": 1},{"type": "bottom","color": "000000","style": 1},{"type": "right","color": "000000","style": 1}]}`)
-		file.SetCellStyle(sheetName, "A12", fmt.Sprintf("I%d", CKPTStartWriteKegiatan+counter), border)
 	}
 
 }
